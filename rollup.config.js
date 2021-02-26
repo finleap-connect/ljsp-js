080ab3b52db47086f7b5dfc8111427e9e8ba2b70
joseph.crick <joseph.crick@finleap.com>
2021-02-26 14:24:28 +0100

    ci(): rollup build
    Detailed explanation:
    
    Removed unused rollup plugins. Updated the
    .npmignore to ignore source directory. Updated
    the rollup build to produce one artifact: CJS.
    
    Reported-by: Joe Crick

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,9 @@
 import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
-import json from "rollup-plugin-json";
 import resolve from "rollup-plugin-node-resolve";
-import external from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
-import url from "rollup-plugin-url";
 // If you want to run analytics against the bundle size, import analyze
 // as below, and include `analyze()` in the plugins array.
 // import analyze from "rollup-plugin-analyzer";
-
-const sharedSettings = {
-  plugins: [
-    resolve({
-      preferBuiltins: true
-    }),
-    postcss({
-      extract: false,
-      minimize: true
-    }),
-    external({
-      includeDependencies: true
-    }),
-    url(),
-    json(),
-    babel({
-      plugins: [
-        "@babel/plugin-proposal-object-rest-spread",
-        "@babel/plugin-proposal-optional-chaining",
-        "@babel/plugin-syntax-dynamic-import",
-        "@babel/plugin-proposal-class-properties"
-      ],
-      exclude: "node_modules/**"
-    }),
-    commonjs()
-  ],
-  external: ["lodash"]
-};
 
 export default [
   {
@@ -45,13 +13,23 @@ export default [
         file: "dist/index.js",
         format: "cjs",
         sourcemap: false
-      },
-      {
-        file: "dist/index.es.js",
-        format: "es",
-        sourcemap: false
       }
     ],
-    ...sharedSettings
+    plugins: [
+      resolve({
+        preferBuiltins: true
+      }),
+      babel({
+        plugins: [
+          "@babel/plugin-proposal-object-rest-spread",
+          "@babel/plugin-proposal-optional-chaining",
+          "@babel/plugin-syntax-dynamic-import",
+          "@babel/plugin-proposal-class-properties"
+        ],
+        exclude: ["node_modules/**", "lib", "bin"]
+      }),
+      commonjs()
+    ],
+    external: ["lodash"]
   }
 ];
