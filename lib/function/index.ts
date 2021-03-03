96001b80f406100555dce9f5ce694ef36f94d351
ashish.santikari <ashish.santikari@finleap.com>
2021-03-03 14:58:51 +0100

    chore: org imports

---
+++ lib/function/index.ts
@@ -0,0 +1,4 @@
+export { juxt } from "./juxt";
+export { trampoline } from "./trampoline";
+export { complement } from "./complement";
+export { fNil } from "./f-nil";
---
+++ lib/regexp/index.ts
@@ -0,0 +1,1 @@
+export { cat } from "./cat";
