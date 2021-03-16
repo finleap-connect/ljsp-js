09258612b60a3ea500ef43e8764853df43265fc0
joseph.crick <joseph.crick@finleap.com>
2021-03-16 08:00:29 +0100

    feat(): fnext, next, and rest
    Detailed explanation:
    
    Added the `next` and `fnext` functions. Updated the
    `rest` function to more closely match the spec.
    
    Reported-by: Joe Crick

---
+++ lib/list/next.ts
@@ -0,0 +1,17 @@
+import { spec } from "../spec/spec";
+import { empty$ } from "../generic/empty$";
+import { rest } from "./rest";
+import { object$ } from "../generic/object$";
+import { eq } from "../generic/eq";
+import { or } from "../conditional/or";
+
+/**
+ * @param {[]} set
+ * @returns {*}
+ */
+export function next(set: Array<any> | object) {
+  spec({ func: "next", spec: { setIsArrayOrObject: object$(set) } });
+  set = Array.isArray(set) ? set : Object.entries(set);
+  // @ts-ignore
+  return or(empty$(set), () => eq(set.length, 1)) ? undefined : rest(set);
+}
