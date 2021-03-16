e66bfa7ed94b40ccc7018f4019d1fcade63de875
ashish.santikari <ashish.santikari@finleap.com>
2021-03-16 09:23:45 +0100

    feat(): escape

---
+++ lib/string/escape.ts
@@ -0,0 +1,21 @@
+import { get } from "../list";
+import { object$ } from "../generic/object$";
+import { string$ } from "../generic/string$";
+import { spec } from "../spec/spec";
+
+export function escape(baseString: string, cmap: Record<string, string>): string {
+  spec({
+    func: "escape",
+    spec: {
+      baseString: string$(baseString),
+      isValidCmap: object$(cmap)
+    }
+  });
+  let escaped = "";
+  for (let index = 0; index < baseString.length; index++) {
+    const ch = baseString.charAt(index);
+    const replacement = get(cmap, ch);
+    escaped += replacement ?? ch;
+  }
+  return escaped;
+}
