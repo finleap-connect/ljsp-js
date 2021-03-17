76b325ace5f6d8a87cfff9665b547106b3677acd
joseph.crick <joseph.crick@finleap.com>
2021-03-17 07:39:54 +0100

    refactor(): equality comparisons
    Detailed explanation:
    
    Most cases of equality comparison are between
    two items. There's no need to utilise our `_eq`
    function for something so simple. Introduced arg
    checks to determine if we're dealing with a simple
    two-element comparison and, if so, perform that directly
    rather than a more extensive n > 2 comparison.
    
    Also, updated `cond`'s param type.
    
    Reported-by: Joe Crick

--- lib/generic/deep-eq.ts
+++ lib/generic/deep-eq.ts
@@ -1,15 +1,10 @@
 import { isEqual } from "lodash";
-import { spec } from "../spec/spec";
 import { _eq } from "./internal/_eq";
-import { object$ } from "./object$";
-import { typedSet$ } from "./internal/typed-set$";
+import { first } from "../list/first";
+import { second } from "../list/second";
 
 export function deepEq(...rest: Array<any>): boolean {
-  spec({
-    func: "eq",
-    spec: { typeIsStructural: typedSet$(rest, (item) => object$(item) || Array.isArray(item)) }
-  });
-  return _eq(rest, deepNotEqual);
+  return rest.length === 2 ? !isEqual(first(rest), second(rest)) : _eq(rest, deepNotEqual);
 }
 
 function deepNotEqual(left: any, right: any): boolean {
--- lib/generic/eq.ts
+++ lib/generic/eq.ts
@@ -1,15 +1,9 @@
-import { spec } from "../spec";
 import { _eq } from "./internal/_eq";
-import { object$ } from "./object$";
-import { typedSet$ } from "./internal/typed-set$";
-import { Primitive } from "../types/primitive";
+import { first } from "../list/first";
+import { second } from "../list/second";
 
-export function eq(...rest: Array<Primitive>) {
-  spec({
-    func: "eq",
-    spec: { typeIsPrimitive: typedSet$(rest, (item) => !object$(item) && !Array.isArray(item)) }
-  });
-  return _eq(rest, simpleNotEqual);
+export function eq(...rest: Array<any>) {
+  return rest.length === 2 ? first(rest) === second(rest) : _eq(rest, simpleNotEqual);
 }
 
 function simpleNotEqual(left: any, right: any) {
