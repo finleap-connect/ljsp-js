2729799646cc90cf5d7130dde1e33309e4b5657c
joseph.crick <joseph.crick@finleap.com>
2021-03-22 10:58:44 +0100

    refactor(): interpose
    Updated `interpose` to return a `transducer` if called with
    appropriate arguments. Added tests for `interpose`.
    
    Reported-by: Joe Crick

--- lib/list/interpose.ts
+++ lib/list/interpose.ts
@@ -1,22 +1,25 @@
-import { isEmpty } from "lodash";
+import { reduced$ } from "./reduced$";
+import { void$ } from "../generic/void$";
+import { notEmpty$ } from "../generic/not-empty$";
 
-/**
- * @param {*} sep
- * @param {[]} set
- * @returns {(function(*): *)|*}
- */
-// @ts-expect-error ts-migrate(7006) FIXME: Parameter 'sep' implicitly has an 'any' type.
-export function interpose(sep, set) {
-  // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'set' implicitly has an 'any' type.
-  function run(set) {
-    // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'acc' implicitly has an 'any' type.
-    return set.reduce((acc, cur) => {
-      return !isEmpty(acc) ? acc.concat([sep, cur]) : [cur].concat(sep);
-    }, []);
-  }
-  if (!set) {
-    return run;
+export function interpose(sep: any, set?: any[]) {
+  if (void$(set)) {
+    return function (step: Function) {
+      let started = false;
+      return function (result: any[], input: any) {
+        if (started) {
+          let sepr = step(result, sep);
+          return reduced$(sepr) ? sepr : step(sepr, input);
+        } else {
+          started = true;
+          return step(result, input);
+        }
+      };
+    };
   } else {
-    return run(set);
+    // @ts-ignore
+    return set.reduce((acc: any[], cur) => {
+      return notEmpty$(acc) ? acc.concat([sep, cur]) : [cur];
+    }, []);
   }
 }
